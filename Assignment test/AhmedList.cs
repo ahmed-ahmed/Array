5640474f4512771a0a30b8a5ee812801cc1e140e
mohamed <m@m.m>
2016-07-05 21:18:36 +0200

    Files Created

---
+++ Assignment test/AhmedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_test
+{
+    class AhmedList
+    {
+        public int[] myArray = new int[1];
+        int i;
+
+
+
+
+
+        //ADD
+        public void add(int x)
+        {
+            int[] y = new int[myArray.Length + 1];
+
+            for (i = 0; i < myArray.Length; i++)
+            {
+                y[i] = myArray[i];
+            }
+            y[i] = x;
+
+
+            // el mafrod hna a5ly myArray length = y length w b3deen el for deh bs msh 3arf ezay
+            for (i = 0; i < y.Length; i++)
+            {
+                myArray[i] = y[i];
+            }
+        }
+
+
+
+        // Get
+
+
+
+        // Size
+
+
+        
+
+
+    }
+}
---
+++ Assignment test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new AhmedList();
+
+            test.add(1);
+
+            Console.WriteLine(test);
+
+
+        }
+    }
+}
